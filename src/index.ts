a2525321ffbfd4189caf5fa09b54ac27d0893178
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-01-18 20:30:45 +0100

    fix prettier

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { GraphQLServer } from 'graphql-yoga';
-import { data } from './data';
-import { resolvers } from './resolvers';
+import { GraphQLServer } from 'graphql-yoga'
+import { data } from './data'
+import { resolvers } from './resolvers'
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
   context: { data }
-} as any);
+} as any)
 
-server.start(() => console.log(`👉  http://localhost:4000`));
+server.start(() => console.log(`👉  http://localhost:4000`))
