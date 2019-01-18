930839eaa7d9551a19ca58f206fdff3d1ec33eac
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-01-18 22:07:21 +0100

    add apollo engine

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { ApolloEngine } from 'apollo-engine'
 import { GraphQLServer } from 'graphql-yoga'
 import { data } from './data'
 import { resolvers } from './resolvers'
+
+const port = process.env.PORT || 4000
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -8,4 +11,32 @@ const server = new GraphQLServer({
   context: { data }
 } as any)
 
-server.start(() => console.log(`👉  http://localhost:4000`))
+if (process.env.APOLLO_ENGINE_KEY) {
+  const engine = new ApolloEngine({
+    apiKey: process.env.APOLLO_ENGINE_KEY
+  })
+
+  const httpServer = server.createHttpServer({
+    tracing: true,
+    cacheControl: true
+  })
+
+  engine.listen(
+    {
+      port,
+      httpServer,
+      graphqlPaths: ['/']
+    },
+    () =>
+      console.log(
+        `Server with Apollo Engine is running on http://localhost:${port}`
+      )
+  )
+} else {
+  server.start(
+    {
+      port
+    },
+    () => console.log(`👉  http://localhost:4000`)
+  )
+}
